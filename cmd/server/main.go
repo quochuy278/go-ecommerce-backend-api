9d5c71414a0250939d09e820cc69e7055b7d4554
quochuy278 <quochuy270800@gmail.com>
2024-06-02 18:36:51 +0300

    modify new main func

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"github.com/quochuy278/go-ecommerce-backend-api/internal/routers"
+)
 
 func main() {
-	fmt.Println("Starting go server")
+	r := routers.NewRouter()
+
+	r.Run(":8002") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
